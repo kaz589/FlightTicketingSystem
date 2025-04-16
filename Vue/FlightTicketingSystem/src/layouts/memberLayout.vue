4c0aac39f7ae582086d19e74d9c9114974a65aa0
Tsai <965302@gmail.com>
2025-04-16 10:38:25 +0800

    選位再開發

--- Vue/FlightTicketingSystem/src/layouts/memberLayout.vue
+++ Vue/FlightTicketingSystem/src/layouts/memberLayout.vue
@@ -1,70 +1,65 @@
 <template>
   <v-app>
-    <v-app-bar>
-      <header class="grid grid-cols-3 items-center py-6">
+    <v-app-bar class="app-bar">
+      <!-- Left: Logo -->
+      <v-app-bar-title>
+        <img
+          src="@/assets/Easytrip_text.png"
+          alt="Easytrip Logo"
+          width="25%"
+          height="40%"
+        />
+      </v-app-bar-title>
 
-        <!-- Left: Logo -->
-        <div class="flex justify-start">
-          <img
-            src="@/assets/Easytrip_text.png"
-            alt="Easytrip Logo"
-            width="40%"
-            height="40%"
-          />
-        </div>
+      <!-- Middle: Navigation -->
+      <!-- Tabs -->
+      <v-tabs align-tabs="title" class="tabs">
+        <v-tab v-for="tab in tabs" :text="tab.name" @click="$router.push(tab.path)">
+        
+            <!-- Icon -->
+            <v-icon :class="['mdi', tab.icon]" style="font-size: 30px;"></v-icon>
+            <!-- Name -->
+            <span style="font-size: 25px">{{ tab.name }}</span>
+          
+        </v-tab>
+      </v-tabs>
 
-        <!-- Middle: Navigation -->
-        <!-- Tabs -->
-        <div
-          class="flex justify-start space-x-6 text-base font-semibold mb-1 mt-4"
-        >
-          <div v-for="tab in tabs" :key="tab.name">
-            <v-btn @click="$router.push(tab.path)">
-              <!-- Icon -->
-              <i :class="['mdi', tab.icon]"></i>
-              <!-- Name -->
-              <span>{{ tab.name }}</span>
-            </v-btn>
-          </div>
+      <!-- Right: Language & Avatar -->
+      <div class="flex justify-end items-center space-x-4 text-lg">
+        <span class="mdi mdi-web text-[30px]"></span>
+        <span
+          ><img src="https://flagcdn.com/tw.svg" width="30" alt="Taiwan"
+        /></span>
+        <span>TWD</span>
+        <v-btn class="text-none me-2" height="48" icon slim>
+          <v-avatar color="surface-light" class="mdi mdi-account" size="32" />
 
-        </div>
+          <v-menu v-model="menuVisible" activator="parent" persistent>
+            <v-list density="compact" nav>
+              <v-list-item
+                append-icon="mdi mdi-login"
+                link
+                title="管理員登入"
+                @click="router.push('/login')"
+              />
+              <v-list-item
+                append-icon="mdi mdi-account"
+                link
+                title="個人頁面"
+              />
 
-        <!-- Right: Language & Avatar -->
-        <div class="flex justify-end items-center space-x-4 text-base">
-          <span class="mdi mdi-web text-[30px]"></span>
-          <span
-            ><img src="https://flagcdn.com/tw.svg" width="30" alt="Taiwan"
-          /></span>
-          <span>TWD</span>
-          <v-btn class="text-none me-2" height="48" icon slim>
-            <v-avatar color="surface-light" class="mdi mdi-account" size="32" />
-
-            <v-menu v-model="menuVisible" activator="parent" persistent>
-              <v-list density="compact" nav>
-                <v-list-item
-                  append-icon="mdi mdi-login"
-                  link
-                  title="管理員登入"
-                  @click="router.push('/login')"
-                />
-                <v-list-item
-                  append-icon="mdi mdi-account"
-                  link
-                  title="個人頁面"
-                />
-
-                <v-list-item
-                  append-icon="mdi-logout"
-                  link
-                  title="登出"
-                  @click="logoutChange()"
-                />
-              </v-list>
-            </v-menu>
-          </v-btn>
-        </div>
-      </header>
+              <v-list-item
+                append-icon="mdi-logout"
+                link
+                title="登出"
+                @click="logoutChange()"
+              />
+            </v-list>
+          </v-menu>
+        </v-btn>
+      </div>
     </v-app-bar>
+<br>
     <v-main>
       <div class="pa-4">
         <router-view></router-view>
@@ -76,6 +71,7 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router"; // 引入 vue-router
+
 const router = useRouter(); // 使用 vue-router
 const tabs = ref([
   {
@@ -94,4 +90,17 @@ function logoutChange() {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.app-bar {
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.tabs {
+  display: flex;
+  justify-content: center;
+  flex-grow: 1;
+}
+</style>
---
+++ Vue/FlightTicketingSystem/src/page/member/SeatPayment.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+這是結帳頁面
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
